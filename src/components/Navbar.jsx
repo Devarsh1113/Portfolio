695c5cfc119bee4b208018b002b253c54dd7e11d
Devarsh Agrawal <agrawadel@sheridancollege.ca>
2024-12-23 16:37:22 -0500

    Added Links to NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,33 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
-
-
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="m-8 w-20"src={logo2} alt="logo" />
+        <img className="m-8 w-20" src={logo2} alt="logo" />
       </div>
       <div className="m-8 justify-center flex items-center gap-5 text-3xl text-white">
-       <FaLinkedin />
-       <FaGithub />
-       <FaInstagram />
+        <a
+          href="https://www.linkedin.com/in/devarshagrawal"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-500 transition-colors"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/Devarsh1113"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-500 transition-colors"
+        >
+          <FaGithub />
+        </a>
+        
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
